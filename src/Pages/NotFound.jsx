98521ee7b0e25b8710d95e9b3dc3e252fd553c27
Gurinzada <augustoinacio243@gmail.com>
2024-05-18 17:24:31 -0300

    Finish

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,5 +1,11 @@
+import styles from "../styles/NotFound.module.scss"
+
 export default function NotFound(){
     return(
-        <h1>We're bulding this one!</h1>
+        <div className={styles.ContentError}>
+        <h1 className={styles.TitleError}>ERROR 404 NOT FOUND</h1>
+        <h2 className={styles.Bulding}>We don't have this one yet!</h2>
+        </div>
+
     )
 }
--- src/Pages/Statistic.jsx
+++ src/Pages/Statistic.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import styles from "../styles/Statistic.module.scss"
 
 const getDatas = async () => {
     const response = await fetch(`http://localhost:3000/projects`)
@@ -16,6 +17,7 @@ export default function Statistics(){
     const [Quality, setQA] = useState([])
     const [Test, setTes] = useState([])
     const [Most, setMost] = useState([])
+    const [Minus, setMinus] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,7 +25,6 @@ export default function Statistics(){
             setProjects(data)
             handleTypes(data)
             handleExpensive(data)
-            console.log(projects)
         }
         
         fetchData()
@@ -38,9 +39,11 @@ export default function Statistics(){
         })
         getTheHigh.sort((a,b) => a.Value - b.Value)
         const JustOne = []
+        const JustMinus = []
         JustOne.push(getTheHigh[getTheHigh.length-1])
         setMost(() => JustOne)
-        console.log(JustOne)
+        JustMinus.push(getTheHigh[0])
+        setMinus(() => JustMinus)
     }
 
     const handleTypes = (data) => {
@@ -87,11 +90,12 @@ export default function Statistics(){
 
 
     return(
-        <section>
+        <section className={styles.ContentStatistic}>
+            <div className={styles.AllProjects}>
             <div>
                 <h1>We have {projects.length} projects!</h1>
             </div>
-            <div>
+            <div className={styles.ProjectsLength}>
             <h1>Our Statistic Projects</h1>
             <div>
                 <h3>Back-End Projects</h3>
@@ -122,18 +126,34 @@ export default function Statistics(){
                 <span>{Test.length}</span>
             </div>
             </div>
+            </div>
             <div>
                 <h1>Most expensive Project</h1>
                 <div>
                     <>
-                    {Most.map((item) => (
+                    {Most.length > 0 ? Most.map((item) => (
                         <h3>{item.Name}</h3>
-                    ))}
+                    )):<h4>We don't have any Projects here!</h4>}
                     </>
                     <>
-                    {Most.map((item) => (
+                    {Most.length > 0 ? Most.map((item) => (
                         <span>R${item.Value}</span>
-                    ))}
+                    )):null}
+                    </>
+                </div>
+            </div>
+            <div>
+                <h1>Project with lower value</h1>
+                <div>
+                    <>
+                    {Minus.length > 0 ? Minus.map((item) => (
+                    <h3>{item.Name}</h3>
+                    )):null}
+                    </>
+                    <>
+                    {Minus.length > 0 ? Minus.map((item) => (
+                    <span>R${item.Value}</span>
+                    )):null}
                     </>
                 </div>
             </div>
